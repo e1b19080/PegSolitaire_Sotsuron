91d1220af0b296e174a7f120c41387c0751291d5
e1b19080 <e1b19080@oit.ac.jp>
2024-06-07 10:07:33 +0900

    Mainクラス，Gridクラスの作成
    Mainクラス：ピンの初期配置と状態をGridクラスを用いて指定．
    Gridクラス：縦横のマス数，ピンの有無，マスの状態のデータを格納するgridインスタンスを作成する．

---
+++ Main.java
@@ -0,0 +1,20 @@
+// Todo
+//・sotsuron.javaでできたとこまでGitCraken使いながら複製する．
+//・違うクラスを別ファイルで作る方法
+//・17行目引数力業すぎわろた．
+
+
+public class Main {
+    public static void main(String[] args){
+        
+        //grid:格子点．
+        //pin:その格子点のピンの状態，-1=無し，1=あり，(0=盤面外)
+        //state:動かす先の候補に入っていたら1,それ以外は0.    
+        Grid grid = new Grid();
+        grid.pin = new int[]{-1,1,-1,-1,1,-1,-1,-1,-1};
+        grid.state = new int[]{0,0,0,0,0,0,0,0,0};
+        
+        //Confirm confirm = new Confirm();
+        //confirm.confirmPin(grid);
+    }
+}
